8bcf2410ba6a8f6c1e4bb36f0823f2b656e659b7
Luca Lischetti <luca@sirlisko.com>
2018-06-18 22:15:47 +0100

    improved map responsiveness

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,15 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'proptypes';
 import styled from 'styled-components';
+
+import WindowSizeListener from 'react-window-size-listener';
 
 import Toast from 'tui-chart';
 import 'tui-chart/dist/maps/world';
 
 const Chart = styled.div`
   pointer-events: none;
+  padding-left: 5vw;
   div {
     margin: 0 auto;
   }
@@ -44,11 +47,25 @@ class Map extends PureComponent {
       data: 1,
     }));
 
-    Toast.mapChart(container, { series }, this.options);
+    this.chart = Toast.mapChart(container, { series }, this.options);
   }
 
   render() {
-    return <Chart id="chart-area" />;
+    return (
+      <Fragment>
+        <WindowSizeListener
+          onResize={({ windowWidth }) => {
+            const width = windowWidth > 900 ? 900 : windowWidth;
+            this.chart &&
+              this.chart.resize({
+                width: width,
+                height: width / 1.2,
+              });
+          }}
+        />
+        <Chart id="chart-area" />
+      </Fragment>
+    );
   }
 }
 
